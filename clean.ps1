0a4c19c97f638b35bca042040d50e182bb329244
sharktide <meherrihaan@gmail.com>
2025-08-18 16:33:18 -0400

    Remove PWSH script

--- clean.ps1
+++
@@ -1,4 +0,0 @@
-Get-ChildItem -Recurse -Filter *.ts | ForEach-Object {
-  $base = $_.FullName -replace '\.ts$', ''
-  Remove-Item "$base.js","$base.js.map","$base.d.ts" -ErrorAction SilentlyContinue
-}
