b741402e745e8984a7c4516051d89f93a13cdc45
sharktide <sharktidedev@gmail.com>
2025-10-29 13:04:51 -0400

    Emergency Hotfix 1.0.4

--- main.ts
+++ main.ts
@@ -20,7 +20,7 @@ const ollamaHandlers = require("./node-apis/ollama");
 const utilsHandlers = require("./node-apis/utils");
 const authHandlers = require("./node-apis/auth")
 const spaces = require('./node-apis/spaces');
-const fixPath = require("fix-path");
+const fixPath = require("fix-path").default;
 fixPath();
 
 let mainWindow: any = null;
