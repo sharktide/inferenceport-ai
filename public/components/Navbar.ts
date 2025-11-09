9225e6ede43fc32cc9fa77c0d45c73bcf66a572d
sharktide <sharktidedev@gmail.com>
2025-11-09 10:07:30 -0500

    Modular components

---
+++ public/components/Navbar.ts
@@ -0,0 +1,35 @@
+export class RootNavbar extends HTMLElement {
+        connectedCallback() {
+
+        this.innerHTML = `
+            <nav>
+                <div class="logo">⚡InferencePort AI</div>
+                <ul class="nav-links">
+                    <li><a href="index.html">Dashboard</a></li>
+                    <li><a href="marketplace.html">Marketplace</a></li>
+                    <li><a href="installed.html">Installed</a></li>
+                    <li><a href="settings.html">Settings</a></li>
+                </ul>
+            </nav>
+        `;
+    }
+}
+export class Type1Navbar extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+            <nav>
+                <div class="logo">⚡InferencePort AI</div>
+                <ul class="nav-links">
+                    <li><a href="../index.html">Dashboard</a></li>
+                    <li><a href="../marketplace.html">Marketplace</a></li>
+                    <li><a href="../installed.html">Installed</a></li>
+                    <li><a href="../settings.html">Settings</a></li>
+                </ul>
+            </nav>
+        `;
+    }
+}
+
+customElements.define("root-navbar", RootNavbar);
+
+customElements.define("type1-navbar", Type1Navbar);
