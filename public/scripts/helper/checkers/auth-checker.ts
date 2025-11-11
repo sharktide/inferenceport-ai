867e94ab5436756b6c13da20ded88fb4089442bf
sharktide <sharktidedev@gmail.com>
2025-11-11 08:46:07 -0500

    Fix Auth Link on Chat Page

--- public/scripts/helper/checkers/auth-checker.ts
+++ public/scripts/helper/checkers/auth-checker.ts
@@ -1,17 +1,27 @@
-export async function isLoggedIn(): Promise<boolean> {
+export async function isLoggedIn(root: number = 0): Promise<boolean> {
   try {
     const result = await window.auth.getSession();
 
     const loggedIn = !!result.session?.access_token;
 
     if (!loggedIn) {
-      window.location.href = "auth.html";
+      if (root === 0) {
+        window.location.href = "auth.html";
+      }
+      else if (root === 1) {
+        window.location.href = "../auth.html";
+      }
     }
 
     return loggedIn;
   } catch (err) {
     console.error("Error checking login:", err);
-    window.location.href = "auth.html";
+    if (root === 0) {
+      window.location.href = "auth.html";
+    }
+    else if (root === 1) {
+      window.location.href = "../auth.html";
+    }
     return false;
   }
 }
