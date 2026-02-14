18b6fced876d1612900472eeabcb482c5e72cc87
R.C.M. <sharktidedev@gmail.com>
2026-02-14 15:00:05 -0500

    Remove verbose method in ZSTD tarball extraction

--- src/scripts/bundler/index.ts
+++ src/scripts/bundler/index.ts
@@ -254,7 +254,7 @@ export class ElectronOllama {
 
 	public async extractTarZst(file: string, outDir: string): Promise<void> {
 		return new Promise((resolve, reject) => {
-			const proc = spawn("tar", ["-xvf", file, "-C", outDir]);
+			const proc = spawn("tar", ["-xf", file, "-C", outDir]);
 
 			proc.on("error", reject);
 			proc.on("close", (code) => {
